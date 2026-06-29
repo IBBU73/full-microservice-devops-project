3d5db4023b637794fbdbc2d384c4b0b8e31d5e8a
IBBU73 <mohamedibrahim80120@gmail.com>
2026-06-29 17:43:19 +0530

    service files added

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,10 @@
+import api from "./api";
+
+export const login = (data) => {
+  return api.post("/user.login", data);
+};
+
+export const register = (data) => {
+  return api.post("./user/register", data);
+};
+ 
